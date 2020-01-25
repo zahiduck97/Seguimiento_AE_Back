40871f2aa69252197ad6df7b7403f2d9a214dfda
zahiduck97 <zahid.butron@gmail.com>
2020-01-24 18:55:07 -0600

    Primer commit ejecutado

---
+++ src/app.js
@@ -0,0 +1,35 @@
+// Requires
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+// Initializations
+const app = express();
+
+// Settings
+const puerto = process.env.PORT || 3000;
+app.set('port', puerto);
+
+// Middlewares
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded( { extended: true } ));
+
+// GLobal Services
+
+
+// Routes
+
+
+// Static Fields
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Server
+app.listen(app.get('port'), () => {
+    console.log('Server on port: ', app.get('port'));
+});
+
