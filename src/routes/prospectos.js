742b1e49a04d03ff383dbfc128ce84f4ae0d002f
Zahiduck <zahid.butron@gmail.com>
2020-02-11 00:43:03 -0600

    se creo prospectos

--- src/routes/prospectos.js
+++ src/routes/prospectos.js
@@ -20,7 +20,7 @@ router.post('/', async(req, res) => {
 
 // Get all empresas
 router.get('/', async(req, res) => {
-    const db = await pool.query(`SELECT * FROM Prospectos`).catch(e => {
+    const db = await pool.query(`SELECT * FROM prospectosView`).catch(e => {
         manejoErrores('Error al buscar los Prospectos', res);
     });
 
