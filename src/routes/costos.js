45f383ddc8fbf22b315ad7799e6c5027d2769a3d
Zahid <zbutron@trigarante.com>
2020-02-28 14:08:21 -0600

    se acabo costos

--- src/routes/costos.js
+++ src/routes/costos.js
@@ -35,6 +35,48 @@ router.get('/', async(req, res) => {
         res.json(db);
 })
 
+// Update
+router.put('/:id', async(req, res) => {
+
+    const id = req.params.id;
+
+    if(!req.body.idNorma || !req.body.idTipoServicio || !req.body.costo){
+        manejoErrores('Faltan Datos', res);
+        return
+    }
+
+    const db = await pool.query(`UPDATE Costos SET idNorma = '${req.body.idNorma}', idTipoServicio = '${req.body.idTipoServicio}', costo = '${req.body.costo}' WHERE id = ${id}`).catch(e => {
+        manejoErrores('Error al actualizar', res);
+        console.log(e);
+    })
+
+    if(db.affectedRows > 0){
+        const select = await pool.query(`SELECT * FROM costosView WHERE id = ${id}`).catch(e => {
+            manejoErrores('Error al buscar', res);
+            console.log(e);
+        })
+        console.log(select[0]);
+    if (select)
+        res.json(select[0]);
+
+    }
+    else
+        manejoErrores('No existe el id', res);
+});
+
+// Get 1
+router.get('/:id', async(req, res) => {
+    const id = req.params.id;
+
+    const db = await pool.query(`SELECT * FROM costosView WHERE id = ${id} ORDER BY id DESC`).catch(e => {
+        manejoErrores('Error al buscar', res);
+    });
+
+    if (db)
+        res.json(db[0]);
+})
+
+
 // Functions
 function manejoErrores(mensaje, res) {
     res.status(400).json({
